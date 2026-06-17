1ae23b6f13af7adc14006310ca4bce3d7f82b496
androinframindintern <androinframindintern@gmail.com>
2026-06-17 17:52:59 +0530

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ChatbotWidget from './components/site/ChatbotWidget';
